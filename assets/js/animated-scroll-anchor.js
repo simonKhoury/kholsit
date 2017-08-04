a0dfb281b2e5aa77857204a67d3b466564a66850
Simon El Khoury <khoury.simon@aut.edu>
2017-08-04 13:36:14 +0300

    second commit

---
+++ assets/js/animated-scroll-anchor.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	
+  $("#navigate").on('click', function(event) {
+	  
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    } 
+  });
+    
+  setInterval(function(){
+    $('.ct-btn-scroll').toggleClass("bounce");
+  }, 5000);    
+    
+});
---
+++ index.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>kholsit</title>
+		
+<meta charset="utf-8">	
+<meta name="viewport" content="width=device-width, initial-scale=1">	
+  <link href="https://fonts.googleapis.com/css?family=Karla" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="assets/css/reset.css">
+  	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/media.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css" rel="stylesheet">
+<!--	<link rel="stylesheet" href="css/animated-scroll-anchor.css">-->
+	
+
+</head>
+<body>
+
+
+	
+	<section>
+
+
+			<div class="grid wrapper">
+				<!------------------ LOGO SECTION ---------------->
+				<div class="row">
+					<div class="col-md-12 container-logo">
+						<img src="assets/img/logo.png" alt="logo" class="img-responsive" id="logo">
+					
+
+					</div>
+				</div>
+				<!----------------- Titile SECTION --------------->	
+				<div class="row">
+					<div class="col-md-12 msg">
+						<p class="msg-coming-soon"><span>Our website is </span><span> coming soon!</span></p>
+						<p class="available"><span class="eng">NOW AVAILABLE</span><span class="arabic"> متوفر الآن</span></p>		
+					</div>
+				</div>
+				
+				<!----------------- Store SECTION --------------->	
+				
+				<div class="row">
+					<div class="container-img">
+						<a class="link" target="_blank" href="#">
+							<img src="assets/img/app-store.png" class="img-responsive" alt="App Store"></a>
+						<a class="link" target="_blank" href="#">
+						<img src="assets/img/google-play.png" class="img-responsive second-img" alt="Google Play">
+						</a>
+
+					</div>
+				</div>
+				
+				
+						
+						
+					
+					
+			</div>
+			<!----------------- Navigation SECTION --------------->	
+			<div class="row">
+				<div class="col-xs-2-offset-5">
+					<a href="#second_section" id="navigate">
+						<div class="scroll_down">
+							<div id="svg1">
+						
+								<svg version="1.1" id="Mouse" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewbox="0 0 130 231" enable-background="new 0 0 130 231" xml:space="preserve">
+									<g>
+										<path fill="#7F7F7F" d="M72.5,231h-15C25.794,231,0,205.206,0,173.5v-116C0,25.794,25.794,0,57.5,0h15 C104.206,0,130,25.794,130,57.5v116C130,205.206,104.206,231,72.5,231z M57.5,5C28.551,5,5,28.551,5,57.5v116 C5,202.448,28.551,226,57.5,226h15c28.948,0,52.5-23.552,52.5-52.5v-116C125,28.551,101.448,5,72.5,5H57.5z" />
+									</g>
+									<g id="mouse-circle">
+										<circle fill-rule="evenodd" clip-rule="evenodd" fill="#7F7F7F" cx="65" cy="42.834" r="12.5" />
+									</g>
+								</svg>
+						
+						
+							</div>
+						
+					
+							
+							<div id="multiple1" >
+							
+								<div class="scroll-arrow-container arrow1">
+									<span class="arrow-left"></span>
+									<span class="arrow-right"></span>
+								</div>
+								<div class="scroll-arrow-container arrow2">
+									<span class="arrow-left"></span>
+									<span class="arrow-right"></span>
+								</div>
+							
+					
+						</div>
+					</div>
+				</a>
+			</div>
+		</div>
+	
+	</section>
+	<!---------------------------- SECTION 2 -------------------------------->
+	<section id="second_section">
+		
+		
+			<div class="grid main">
+				<!----------------- TITLE ------------->
+				<div class="row">
+					<div class="col-xs-12 col-md-12 col-lg-12 title">
+						<p>WHAT'S KHOLSIT ?</p>
+					</div>
+				</div>
+				
+				<div class="row media_section">
+					
+						<div class="col-xs-12 col-md-12 col-lg-6 first_half">
+	<!--						first_half-->
+
+							<div class="embed-responsive embed-responsive-16by9" id="video_box_1">
+								<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/zpOULjyy-n8" frameborder="0" id="iframe"></iframe>
+
+							</div>
+							<p id="for_who">FOR HANDYMAN</p>
+						</div>
+					
+					
+						<div class="col-xs-12 col-md-12 col-lg-6 second_half">
+	<!--						second_half-->
+							<div class="embed-responsive embed-responsive-16by9" id="video_box_2">
+								<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/EitmSZ488RA" frameborder="0" id="iframe"></iframe>
+
+							</div>
+							<p id="for_who" >FOR YOU</p>
+						</div>
+					
+                      
+						
+				</div>
+			</div>
+				
+			
+			
+			
+		
+<div class="page-footer"><p id="footer">KHOLSIT 2017</p></div>
+				
+				
+		
+		
+	</section>
+
+
+  
+  
+</body>
+</html>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<script src="assets/js/animated-scroll-anchor.js" type="text/javascript"></script>
