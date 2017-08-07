9c8e39513a4a9605d5b3764218a855211cbbd097
Simon El Khoury <khoury.simon@aut.edu>
2017-08-07 14:56:51 +0300

    3rd commit

--- index.php
+++ index.php
@@ -4,79 +4,107 @@
   <title>kholsit</title>
 		
 <meta charset="utf-8">	
-<meta name="viewport" content="width=device-width, initial-scale=1">	
-  <link href="https://fonts.googleapis.com/css?family=Karla" rel="stylesheet">
-	<link rel="stylesheet" type="text/css" href="assets/css/reset.css">
-  	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
-	<link rel="stylesheet" type="text/css" href="assets/css/media.css">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-	<link href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css" rel="stylesheet">
-<!--	<link rel="stylesheet" href="css/animated-scroll-anchor.css">-->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+<meta name="description" content="Requesting a handyman is easy on “Kholsit”. Find a  handymen by location, price and rating and schedule an appointment at your convenience">
+<meta name="MobileOptimized" content="width">
+<meta name="HandheldFriendly" content="true">
+<meta name="apple-mobile-web-app-capable" content="yes">
+	
+<meta name="DC.title" content="Kholsit - We Get Things Done!">
+<meta name="keywords" content="Kholsit, We Get Things Done, The Handymen Directory, Lebanon Handyman, Handyman In Lebanon, Lebanese Startups">
+<meta property="og:title" content="Kholsit - We Get Things Done!">
+<meta property="og:type" content="website">
+<meta property="og:url" content="http://kholsit.com/">
+<meta property="og:image" content="https://scontent.fbey8-1.fna.fbcdn.net/v/t31.0-8/20023770_1574450185919819_8414764379020921091_o.jpg?oh=415407b15809458773c576f26b52520b&amp;oe=5A29BC77">
+<meta name="description" content="Requesting a handyman is easy on “Kholsit”. Find a  handymen by location, price and rating and schedule an appointment at your convenience">	
+	
+<link href="https://fonts.googleapis.com/css?family=Karla" rel="stylesheet">
+   <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    	<link href="assets/css/reset.css">
+    		<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+				<link href="assets/css/media.css" type="text/css" rel="stylesheet">
+	
+    
 	
 
 </head>
 <body>
 
-
+<div class="container-fluid">
 	
 	<section>
-
-
-			<div class="grid wrapper">
-				<!------------------ LOGO SECTION ---------------->
+		<div class="container">
+			<!------------------ LOGO SECTION ---------------->
 				<div class="row">
-					<div class="col-md-12 container-logo">
-						<img src="assets/img/logo.png" alt="logo" class="img-responsive" id="logo">
-					
+					<div class="col-md-12 logo">
+						<div class="image-logo">
+							<img src="assets/img/logo.png" alt="logo" class="img-responsive" id="logo">
+						</div>
 
 					</div>
 				</div>
 				<!----------------- Titile SECTION --------------->	
 				<div class="row">
 					<div class="col-md-12 msg">
-						<p class="msg-coming-soon"><span>Our website is </span><span> coming soon!</span></p>
-						<p class="available"><span class="eng">NOW AVAILABLE</span><span class="arabic"> متوفر الآن</span></p>		
+						<div class="coming-soon text">
+							<h3>
+								<span>Our website is </span><span>coming soon!</span></p>
+							</h3>
+							<h3>
+								<span class="eng text-uppercase">now available</span>
+								<span class="arabic"> متوفر الآن</span>
+							</h3>
+							
+						</div>
 					</div>
 				</div>
 				
 				<!----------------- Store SECTION --------------->	
 				
-				<div class="row">
-					<div class="container-img">
-						<a class="link" target="_blank" href="#">
-							<img src="assets/img/app-store.png" class="img-responsive" alt="App Store"></a>
-						<a class="link" target="_blank" href="#">
-						<img src="assets/img/google-play.png" class="img-responsive second-img" alt="Google Play">
-						</a>
-
-					</div>
+				<div class="row" id="no-margin">
+					<div class="col-md-12">
+						<div class="store">
+							<div class="row" id="inner-row" >
+								<div class="col-md-6 col-sm-6 col-xs-12">
+									<div class="app-store">
+										
+										<a class="link" target="_blank" href="#">
+											<img src="assets/img/app-store.png" class="img-responsive" alt="App Store">
+										</a>
+									</div>
+									
+								</div>
+								
+								<div class="col-md-6 col-sm-6 col-xs-12">
+									<div class="google-play-store">
+										
+										<a class="link" target="_blank" href="#">
+											<img src="assets/img/google-play.png" class="img-responsive " alt="Google Play">
+										</a>
+									</div>	
+								</div>
+							</div>
+						</div>
+					</div>	
 				</div>
-				
-				
-						
-						
-					
-					
-			</div>
+		</div>
 			<!----------------- Navigation SECTION --------------->	
-			<div class="row">
-				<div class="col-xs-2-offset-5">
+
+			<div class="navigate">
+				
 					<a href="#second_section" id="navigate">
 						<div class="scroll_down">
 							<div id="svg1">
-						
-								<svg version="1.1" id="Mouse" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewbox="0 0 130 231" enable-background="new 0 0 130 231" xml:space="preserve">
+								<svg version="1.1" id="Mouse" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 130 231" enable-background="new 0 0 130 231" xml:space="preserve">
 									<g>
-										<path fill="#7F7F7F" d="M72.5,231h-15C25.794,231,0,205.206,0,173.5v-116C0,25.794,25.794,0,57.5,0h15 C104.206,0,130,25.794,130,57.5v116C130,205.206,104.206,231,72.5,231z M57.5,5C28.551,5,5,28.551,5,57.5v116 C5,202.448,28.551,226,57.5,226h15c28.948,0,52.5-23.552,52.5-52.5v-116C125,28.551,101.448,5,72.5,5H57.5z" />
+										<path fill="#7F7F7F" d="M72.5,231h-15C25.794,231,0,205.206,0,173.5v-116C0,25.794,25.794,0,57.5,0h15 C104.206,0,130,25.794,130,57.5v116C130,205.206,104.206,231,72.5,231z M57.5,5C28.551,5,5,28.551,5,57.5v116 C5,202.448,28.551,226,57.5,226h15c28.948,0,52.5-23.552,52.5-52.5v-116C125,28.551,101.448,5,72.5,5H57.5z"></path>
 									</g>
 									<g id="mouse-circle">
-										<circle fill-rule="evenodd" clip-rule="evenodd" fill="#7F7F7F" cx="65" cy="42.834" r="12.5" />
+										<circle fill-rule="evenodd" clip-rule="evenodd" fill="#7F7F7F" cx="65" cy="42.834" r="12.5"></circle>
 									</g>
 								</svg>
-						
-						
 							</div>
-						
 					
 							
 							<div id="multiple1" >
@@ -95,64 +123,83 @@
 					</div>
 				</a>
 			</div>
-		</div>
+		
+
 	
 	</section>
 	<!---------------------------- SECTION 2 -------------------------------->
-	<section id="second_section">
+	<section id="second_section" class="color-white">
 		
+		<div class="row" id="title">
+					<div class="col-md-12">
+						<div class="text-uppercase text-center"><span class="color-white">WHAT'S KHOLSIT ?</span></div>
+					</div>
+			</div>
 		
-			<div class="grid main">
-				<!----------------- TITLE ------------->
-				<div class="row">
-					<div class="col-xs-12 col-md-12 col-lg-12 title">
-						<p>WHAT'S KHOLSIT ?</p>
+		<div id="second_container">
+			
+			
+			
+			<div class="row">
+				
+				<div class="col-md-12 first_half">
+				
+					<div class="media_section">
+						<div class="row" id="row-media">
+					
+							<div class="col-md-6 col-sm-6 col-xs-12 first_half">
+	
+
+								<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/fX4xJ8lwXow" frameborder="0" allowfullscreen=""></iframe>
+								<div class="text-uppercase">
+									<span>for handyman</span>
+								</div>
+								
+							</div>
+
+
+							<div class="col-md-6 col-sm-6 col-xs-12 first_half">
+		
+
+								<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/fX4xJ8lwXow" frameborder="0" allowfullscreen=""></iframe>
+								<div class="text-uppercase">			<span>for you</span>
+								</div>
+								
+							</div>
+						</div>
 					</div>
 				</div>
+			</div>
+		</div>
+	
+	
+	
+	
+	</section>
+	
+	<footer>
+		<div class="panel-footer">
+			
+			<div class="row">
+				<div class="col-md-12">
+					<div class="text-uppercase text-center">
+						<span>kholsit 2017</span>
+					</div>
 				
-				<div class="row media_section">
-					
-						<div class="col-xs-12 col-md-12 col-lg-6 first_half">
-	<!--						first_half-->
-
-							<div class="embed-responsive embed-responsive-16by9" id="video_box_1">
-								<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/zpOULjyy-n8" frameborder="0" id="iframe"></iframe>
-
-							</div>
-							<p id="for_who">FOR HANDYMAN</p>
-						</div>
-					
-					
-						<div class="col-xs-12 col-md-12 col-lg-6 second_half">
-	<!--						second_half-->
-							<div class="embed-responsive embed-responsive-16by9" id="video_box_2">
-								<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/EitmSZ488RA" frameborder="0" id="iframe"></iframe>
-
-							</div>
-							<p id="for_who" >FOR YOU</p>
-						</div>
-					
-                      
-						
 				</div>
 			</div>
-				
-			
-			
-			
 		
-<div class="page-footer"><p id="footer">KHOLSIT 2017</p></div>
-				
-				
-		
-		
-	</section>
+		</div>
+	
+	</footer>
+	
 
 
-  
+	</div>
   
 </body>
 </html>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<script src="assets/js/bootstrap.min.js"></script>
 <script src="assets/js/animated-scroll-anchor.js" type="text/javascript"></script>
+
